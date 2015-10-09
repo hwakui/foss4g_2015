87fa30dc4eeec01c9ddebbab18d80d035d60d4c7
hwakui <hisashi-wakui@aeroasahi.co.jp>
2015-10-09 12:44:02 +0900

    r3

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -5,3 +5,53 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 
 viewer.camera.flyTo({ destination : Cesium.Cartesian3.fromDegrees(139.50022, 35.91546, 15000.0)});
 
+//ƒ|ƒCƒ“ƒg‚ð’Ç‰Á
+
+var mainpoint = viewer.entities;
+
+
+mainpoint.add({
+
+        position : Cesium.Cartesian3.fromDegrees(139.50022, 35.91546),
+
+        point : {
+
+            pixelSize : 20,
+
+            color : Cesium.Color.BLUE
+        }
+    
+});
+
+var point = viewer.entities;
+
+
+point.add({
+
+        position : Cesium.Cartesian3.fromDegrees(138.59053, 36.35607),
+
+        point : {
+
+            pixelSize : 20,
+
+            color : Cesium.Color.GREEN
+        }
+    
+});
+
+var point2 = viewer.entities;
+
+
+point2.add({
+
+        position : Cesium.Cartesian3.fromDegrees(138.58719, 36.41000),
+
+        point : {
+
+            pixelSize : 10,
+
+            color : Cesium.Color.YELLOW
+
+        }
+    
+});
