b5cf1f73c3be6b6648b9e305195da17218f64801
hwakui <hisashi-wakui@aeroasahi.co.jp>
2015-10-09 11:20:06 +0900

    add point
    ポイント追加

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,13 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
+
+//ƒ|ƒCƒ“ƒg‚ð’Ç‰Á
+
+var point = viewer.entities;
+
+point.add({
+        position : Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+        point : {
+            pixelSize : 10,
+            color : Cesium.Color.YELLOW
+    }
+});
