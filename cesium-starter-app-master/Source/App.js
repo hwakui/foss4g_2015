d94e3fa1a3a893133b4dcad3247567759f67b947
hwakui <hisashi-wakui@aeroasahi.co.jp>
2015-10-09 11:24:44 +0900

    add line
    ラインを追加

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -5,9 +5,35 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 var point = viewer.entities;
 
 point.add({
+
         position : Cesium.Cartesian3.fromDegrees(-75.59777, 40.03883),
+
         point : {
+
             pixelSize : 10,
+
             color : Cesium.Color.YELLOW
+
+        }
+    
+});
+
+//ƒ‰ƒCƒ“‚ð’Ç‰Á
+
+var line1 = viewer.entities;
+
+line1.add({
+
+    name : 'Red line on the surface',
+
+    polyline : {
+
+        positions : Cesium.Cartesian3.fromDegreesArray([-75, 35,
+ -125, 35]),
+
+        width : 5,
+
+        material : Cesium.Color.RED
+
     }
 });
